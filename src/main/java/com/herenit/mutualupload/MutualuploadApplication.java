b3e6f8f771154a5b3cffc378f33be7cfb7f10c3c
Axpb-8 <985974367@qq.com>
2021-09-27 14:04:22 +0800

    项目初始化

---
+++ src/main/java/com/herenit/mutualupload/MutualuploadApplication.java
@@ -0,0 +1,13 @@
+package com.herenit.mutualupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutualuploadApplication.class, args);
+    }
+
+}
